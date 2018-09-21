c5087d36dff96c01a71d2dc48a50bad57fc20ac4
Paulo Filho <pcf.feat@gmail.com>
2018-09-21 19:50:58 -0400

    Drafting some stuff; components inc

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import './style.scss';
+
+new Vue({
+  el: '#app',
+  components: { 
+    'movie-list': {
+      template: `<div id="movie-list">
+                    Movie list
+                 </div>`
+    },
+    'movie-filter': {
+      template: `<div id="movie-filter">
+                    Movie filter
+                 </div>`
+    }
+  }
+});
