f6a64277cc541e8552a94199859c1c396cf153c6
Paulo Filho <pcf.feat@gmail.com>
2018-09-24 19:53:31 -0400

    Expanding components

--- src/main.js
+++ src/main.js
@@ -1,18 +1,41 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
 
 new Vue({
   el: '#app',
   components: { 
     'movie-list': {
       template: `<div id="movie-list">
-                    Movie list
-                 </div>`
+                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                 </div>`,
+      data: function () {
+        return {
+          movies: [
+            { title: 'Pulp Fiction'},
+            { title: 'Home Alone'},
+            { title: 'Austin Powers'}
+          ]
+        };
+      }
     },
     'movie-filter': {
+      data() {
+        return {
+          genres
+        }
+      },
       template: `<div id="movie-filter">
-                    Movie filter
-                 </div>`
+                    <h2>Filter results</h2>
+                    <div class="filter-group">
+                      <check-filter v-for="genre in genres"></check-filter>
+                    </div>
+                 </div>`,
+      components: {
+        'check-filter': {
+          template: `<div>Filter</div>`
+        }
+      }
     }
   }
 });
