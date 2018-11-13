8661a5282b0736e43671e63662a9cd1abac9d484
Paulo Filho <pcf.feat@gmail.com>
2018-11-13 18:56:53 -0300

    Working detail page

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>Hello World</div>
+    <div id="detail" v-if="movie">
+        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+    </div>
 </template>
 <script>
+import MovieItem from './MovieItem.vue';
+
 export default {
-    
+    props: [ 'movies' ],
+    computed: {
+        movie() {
+            let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+            return movie ? movie : null;
+        }
+    },
+    components: {
+        MovieItem
+    },
+    created() {
+
+    }
 }
 </script>
