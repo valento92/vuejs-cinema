d9ae89d5b323e6e1da3b2cd18b15f072184a1d90
Paulo Filho <pcf.feat@gmail.com>
2018-11-14 18:50:40 -0300

    Better approach at child elements with slots, fleshing out detail page, adding home link to detail page

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,20 @@
 <template>
     <div id="detail" v-if="movie">
-        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+        <movie-item v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr><td>Release data: </td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running time: </td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director: </td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast: </td><td>{{ movie.Actors }}</td></tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{ name: 'home' }">Back to results</router-link>
+        </div>
     </div>
 </template>
 <script>
@@ -11,7 +25,7 @@ export default {
     computed: {
         movie() {
             let movie = this.movies.find(movie => movie.id === this.$route.params.id);
-            return movie ? movie : null;
+            return movie ? movie.movie : null;
         }
     },
     components: {
